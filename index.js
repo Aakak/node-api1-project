662c452fd57587d4afdab23ffcfc4b2ebe654d9c
Akak Almazbekova <akak.almazbekova@gmail.com>
2019-10-28 16:10:07 -0700

    created get, post ,put, delete

--- index.js
+++ index.js
@@ -1,111 +1,100 @@
 // implement your API here
-
 const express = require('express');
-// const db = require('./data/db.js');
-// console.log("Hello");
+const db = require('./data/db.js');
 
 
 const server = express();
 server.get('/', (req, res) => {
-    res.send("Hello Node 23!");
+    res.send("My node!");
 })
 const port = 8000;
 server.listen(port, () => console.log("\n=== API server on 8000 ===\n"));
 
+server.use(express.json());
 
-// server.use(express.json());
 
-// server.get('/api/users', (req, res) => {
-//   db.find()
-//     .then(users => res.status(200).json(users))
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json({error: "The users information could not be retrieved."});
-//     });
-// });
+// POST   | /api/users     | Creates a user using the information sent inside the `request body`
 
-// server.post('/api/users', (req, res) => {
-//   console.log(req.body);
-//   const { name, bio } = req.body;
-//   if (!name || !bio) {
-//     res.status(400).json({error: "Please provide name and bio for the user."});
-//   } else {
-//     db.insert({ name, bio })
-//       .then(({ id }) => {
-//         db.findById(id)
-//           .then(user => {
-//             res.status(201).json(user);
-//           })
-//           .catch(err => {
-//             console.log(err);
-//             res.status(500).json({error: "The users information could not be retrieved."});
-//           });
-//       })
-//       .catch(err => {
-//         console.log(err);
-//         res.status(500).json({error: "There was an error while saving the user to the database"});
-//       });
-//   }
-// });
+server.post('/api/users', (req, res) => {
+    console.log(req.body);
+    const { name, bio } = req.body;
+      db.insert({ name, bio })
+        .then(({ id }) => {
+          db.findById(id)
+            .then(user => {
+              res.json(user);
+            })
+            .catch(err => {
+              console.log(err);
+              res.json({error: "error geting users data"});
+            });
+        })
+        .catch(err => {
+          console.log(err);
+          res.json({error: "error geting users data"});
+        });
+    }
+  });
 
-// server.get('/api/users/:id', (req, res) => {
-//   const { id } = req.params;
-//   db.findById(id)
-//     .then(user => {
-//       console.log("user", user);
-//       if (user) {
-//         res.status(200).json(user);
-//       } else {
-//         res.status(404).json({error: "The user with the specified ID does not exist."});
-//       }
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json({error: "The user information could not be retrieved."});
-//     });
-// });
+//   | GET    | /api/users     | Returns an array of all the user objects contained in the database.  
 
-// server.delete('/api/users/:id', (req, res) => {
-//   const { id } = req.params;
-//   db.remove(id)
-//     .then(deleted => {
-//       console.log(deleted);
-//       if (deleted) {
-//         res.status(204).end();
-//       } else {
-//         res.status(404).json({error: "The user with the specified ID does not exist."});
-//       }
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json({error: "The user could not be removed"});
-//     });
-// });
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => res.status(200).json(users))
+    .catch(err => {
+      console.log(err);
+      res.json({error: "error geting users data"});
+    });
+});
 
-// server.put('/api/users/:id', (req, res) => {
-//   const { id } = req.params;
-//   const { name, bio } = req.body;
-//   if (!name && !bio) {
-//     // can use a return to shortcut the funciton instead of an else
-//     return res.status(400).json({error: 'Please provide name and bio for the user.'});
-//   }
-//   db.update(id, { name, bio })
-//     .then(updated => {
-//       if (updated) {
-//         db.findById(id)
-//           .then(user => res.status(200).json(user))
-//           .catch(err => {
-//             console.log(err);
-//             res.status(500).json({error: 'The user informatmion could not be retrieved.'});
-//           });
-//       } else {
-//         res.status(404).json({error: `The user with the specified ID does not exist.`});
-//       }
-//     })
-//     .catch(err => {
-//       console.log(err);
-//       res.status(500).json({error: 'The user information could not be modified.'});
-//     });
-// });
+
+// | GET    | /api/users/:id | Returns the user object with the specified `id`.  
+
+server.get('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(user => {
+      console.log("user", user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.json({error: "error geting users data"});
+    });
+});
+
+// | GET    | /api/users/:id | Returns the user object with the specified `id`.  
+
+server.delete('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(deleted => {
+      console.log(deleted);
+    })
+    .catch(err => {
+      console.log(err);
+      res.json({error: "Can't delete user id"});
+    });
+});
+
+// | PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified document, **NOT the original**. 
+
+server.put('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, bio } = req.body;
+  db.update(id, { name, bio })
+    .then(updated => {
+      db.findById(id)
+          .then(user => res.status(200).json(user))
+          .catch(err => {
+            console.log(err);
+            res.json({error: "error geting users data" });
+          });
+  
+    })
+    .catch(err => {
+      console.log(err);
+      res.json({error: "Can't update users data"});
+    });
+});
 
 
